0daa1fbc249454f34a8342248c68160f754f564a
Peter Mousses <29151865+petermousses@users.noreply.github.com>
2022-04-30 11:10:51 -0700

    Cleaned up code for problem5

--- problem5.py
+++ problem5.py
@@ -3,19 +3,12 @@
 
 	What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
-from functools import cache
-import sys
-
 def checkDivisibility(num, largest_divisor) -> bool:
 	for divisor in range(largest_divisor, 0, -1):
 		if num % divisor != 0: return False
 	return True
 
-@cache
-def recursion(start_num, largest_divisor) -> int:
-	#print(start_num)
-	# if checkDivisibility(start_num, largest_divisor): return start_num
-	# else: return recursion(start_num + 1, largest_divisor)
+def smallestPositiveNumByDivisorHelper(start_num, largest_divisor) -> int:
 	num = start_num
 	while True:
 		if checkDivisibility(num, largest_divisor): return num
@@ -24,10 +17,9 @@ def recursion(start_num, largest_divisor) -> int:
 def smallestPositiveNumByDivisor(largest_divisor):
 	previous_result = 1
 	for num in range(largest_divisor):
-		previous_result = recursion(previous_result, num)
+		previous_result = smallestPositiveNumByDivisorHelper(previous_result, num)
 		print(previous_result)
 
 def main():
-	sys.setrecursionlimit(10**9)
 	print(smallestPositiveNumByDivisor(20))
 if __name__ == '__main__': main()
