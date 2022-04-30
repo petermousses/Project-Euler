d6f8f1665acb723a53d9dbe9e990e1989525627e
Peter Mousses <29151865+petermousses@users.noreply.github.com>
2022-04-30 11:46:48 -0700

    Cleaned up code for problem6

--- problem6.py
+++ problem6.py
@@ -7,27 +7,7 @@
 		3025 - 385 = 2640
 	Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
-from functools import cache
-
-@cache
-def sumOfSquares(num) -> int:
-	sum = 0
-	for nat_num in range(num + 1):
-		sum += nat_num**2
-	return sum
-
-@cache
-def squareOfSum(num):
-	sum = 0
-	for nat_num in range(num + 1):
-		sum += nat_num
-	return sum**2
-
 def main():
 	num_to_run = 100
-	first = sumOfSquares(num_to_run)
-	second = squareOfSum(num_to_run)
-	print(first)
-	print(second)
-	print(second - first)
+	print(sum(i for i in range(num_to_run + 1))**2 - sum(i**2 for i in range(num_to_run + 1)))
 if __name__ == '__main__': main()
